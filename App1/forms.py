f8f4c489789ad87ed7275c6f1d23d7771c4d766c
Manuel Pena <mpenadev@gmail.com>
2022-09-27 18:19:15 -0300

    Agregando mas estilos

--- App1/forms.py
+++ App1/forms.py
@@ -9,16 +9,16 @@ class SuscripcionFormulario(forms.Form):
     dniSuscripciones1 = forms.IntegerField()
     
 class PrestamoFormulario(forms.Form):
-    nombre = forms.CharField(max_length=40)
-    apellido = forms.CharField(max_length=40)
-    dni=forms.IntegerField()
-    fecha=forms.DateField()
-    genero = forms.CharField(max_length=20)
-    situacion = forms.CharField(max_length=20)
-    banco = forms.CharField(max_length=40)
-    email = forms.EmailField()
-    telefono = forms.IntegerField()
-    cantidad = forms.IntegerField()
+    nombre = forms.CharField(max_length=40, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Nombre'}))
+    apellido = forms.CharField(max_length=40, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Apellido'}))
+    dni=forms.IntegerField(widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'D.N.I'}))
+    fecha=forms.DateField(widget=forms.DateInput(attrs={'class':'form-control', 'placeholder':'Fecha Nacimiento'}))
+    genero = forms.CharField(max_length=20, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Genero'}))
+    situacion = forms.CharField(max_length=20, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Situacion (E.j: Monotributista)'}))
+    banco = forms.CharField(max_length=40, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Banco (E.j: Santander)'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control', 'placeholder':'E-Mail'}))
+    telefono = forms.IntegerField(widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Telefono'}))
+    cantidad = forms.IntegerField(widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Cantidad solicitada: '}))
     
 
 class UserRegisterForm(UserCreationForm):
